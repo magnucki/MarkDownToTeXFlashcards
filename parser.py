9750112b0aeef2fdbb8a2644a928915724f5b536
Rico Magnucki <rmagnuck@techfak.uni-bielefeld.de>
2014-11-10 21:54:59 +0100

    Version 0.2 ready for syntax checking

--- parser.py
+++ parser.py
@@ -1,72 +1,119 @@
 # Author Rico Magnucki
 
 import os
+from enum import Enum, unique
 
-input_file = "sampleinput.md"
 
-question_start = "\\begin{karte}{"
-question_end = "\n\end{karte}\n"
-usepackage = "\\usepackage{"
-usepackage_parameter =  "\\usepackage["
-parameter_package_divider = "]{"
+@unique
+class State(Enum):
+    NONE = 0
+    VALID = 1
+    INVALID = 2
+    QUESTION = 3
+    ANSWER = 4
+    SECTION = 5
+    SUBSECTION = 6
 
-closing_brace = "}\n"
 
-packages = [("inputenc", "utf8"), ("fontenc", "T1"), ("libertine","")]
+INPUT_FILE = "sampleinput.md"
+OUTPUT_FILE = ""
 
-documentclass = "\documentclass[a7paper,grid=rear]{Kartei/kartei}\n"
+QUESTION_START = "\\begin{karte}{"
+QUESTION_END = "\n\end{karte}\n"
+USEPACKAGE = "\\usepackage{"
+USEPACKAGE_PARAMETER = "\\usepackage["
+PARAMETER_PACKAGE_DIVIDER = "]{"
+BEGIN_DOCUMENT = "\\begin{document}\n"
+END_DOCUMENT = "\end{document}\n"
+CONTENT = ''
+CLOSING_BRACE = "}\n"
+CURRENT_STATE = State.NONE
+LAST_STATE = State.NONE
 
-tex_file = open("flashcards.tex", "w")
-input_file = "sampleinput.md"
+QUESTION_TAG = "* "
+ANSWER_TAG = "_"
+SECTION_TAG = "# "
+SUBSECTION_TAG = "## "
+
+
+SECTION = "\section*{"
+SUBSECTION = "\subsection*{"
+
+
+PACKAGES = [("inputenc", "utf8"), ("fontenc", "T1"), ("libertine", ""), ("babel", "ngerman")]
+
+DOCUMENTCLASS = "\documentclass[a7paper,grid=rear]{Kartei/kartei}\n"
+
+TEX_FILE = open("flashcards.tex", "w+")
 
 
 def init_tex():
-    tex_file.write(documentclass)
+    TEX_FILE.write(DOCUMENTCLASS)
     load_packages()
-    tex_file.write("\\begin{document}\n")
+    TEX_FILE.write(BEGIN_DOCUMENT)
 
 
 def close_tex():
-    tex_file.write("\end{document}")
-    tex_file.close()
+    TEX_FILE.write(END_DOCUMENT)
+    TEX_FILE.close()
+
 
 def load_packages():
-     for package in packages:
-         tex_file.write(build_package_statement(package[1],package[0]))
+    for package in PACKAGES:
+        TEX_FILE.write(build_package_statement(package[1], package[0]))
+
 
 def build_package_statement(parameter, packagename):
     if parameter:
-        return usepackage_parameter + parameter + parameter_package_divider + packagename + closing_brace
+        return USEPACKAGE_PARAMETER + parameter + PARAMETER_PACKAGE_DIVIDER + packagename + CLOSING_BRACE
     else:
-        return usepackage + packagename + closing_brace
+        return USEPACKAGE + packagename + CLOSING_BRACE
+
+
+def process_file():
+    with open(INPUT_FILE) as INPUT:
+        print("Zeilen gelesen - parse Input")
+        parse_markdown(INPUT.readlines())
+
+
+def parse_markdown(text_input):
+    for string in text_input:
+        if string.startswith(QUESTION_TAG):
+            TEX_FILE.write(QUESTION_START + string.strip('* \n') + CLOSING_BRACE)
+        elif string.startswith(ANSWER_TAG):
+            TEX_FILE.write(string.strip('_\n') + QUESTION_END)
+        elif string.startswith(SECTION_TAG):
+            TEX_FILE.write(SECTION + string.strip('# \n') + CLOSING_BRACE)
+        elif string.startswith(SUBSECTION_TAG):
+            TEX_FILE.write(SUBSECTION + string.strip('#\n') + CLOSING_BRACE)
+
+
+def setCurrentState(prefix):
+    if prefix is QUESTION_TAG:
+        currentState = State.QUESTION
+    elif prefix is ANSWER_TAG:
+        currentState = State.ANSWER
+    elif prefix is SECTION_TAG:
+        currentState = State.SECTION
+    elif prefix is SUBSECTION_TAG:
+        currentState = State.SUBSECTION
+
+
+def validate_input(linePrefix):
+    lastState = CURRENT_STATE
+    setCurrentState(linePrefix)
+    if CURRENT_STATE is State.QUESTION and (
+                        lastState is State.SECTION or lastState is State.SUBSECTION or lastState is State.ANSWER):
+        return True
+    elif CURRENT_STATE is State.ANSWER and lastState is State.QUESTION:
+        return True
+    elif CURRENT_STATE is State.SECTION and (lastState is State.SUBSECTION or lastState.ANSWER):
+        return True
 
 
 init_tex()
-with open(input_file) as input:
-    content = input.readlines()
-
-    for string in content:
-        if (string.startswith("* ")):
-            tex_file.write(question_start + string.strip('* \n') + closing_brace)
-        if (string.startswith("_")):
-            tex_file.write(string.strip('_\n') + question_end)
-        if (string.startswith("# ")):
-            tex_file.write("\section{" + string.strip('# \n') + closing_brace)
-
-
-
-
-
-
-
+process_file()
 close_tex()
 
-#Run pdflatex to create pdf files
-os.system("pdflatex flashcards.tex")
-
-# \setcardpagelayout
-# \begin{karte}[Fach]
-# {Frage}
-# [Kommentar]
-# Antwort
-# \end{karte}
+# Run pdflatex to create pdf files
+# os.system("pdflatex flashcards.tex")
